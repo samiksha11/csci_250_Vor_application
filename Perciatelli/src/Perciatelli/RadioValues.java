86711bde7b24274e75ca2655d352c82fa47240bb
samiksha11 <samiksha.solanki11@gmail.com>
2014-11-07 23:13:35 -0800

    Radio Class , function to  generate radial.

--- Perciatelli/src/Perciatelli/RadioValues.java
+++ Perciatelli/src/Perciatelli/RadioValues.java
@@ -30,38 +30,6 @@ public class RadioValues
                 return 360 - pinRadial;
                 
 	}
-	
-	public int generateID()
-        {
-		Random rnd = new Random();
-                int a = rnd.nextInt(spotID1-spotID2) + spotID2;
-                System.out.println(a);
-               
-		return a;
-	}
-	
-	public String generateSignalQuality()
-        {
-		if(counter <= 0)
-                {
-			return "Good signal";
-		}
-		if(this.counter < 5)
-                {
-			this.counter--;
-			System.out.println(""
-                                + "counter = " + this.counter);
-			return "bad signal";
-		}
-		
-		Random rn4 = new Random();
-		int a = rn4.nextInt(3);
-		if(a == 0)
-                {
-			this.counter--;
-			System.out.println("counter = " + this.counter);
-			return "bad signal";
-		}
-		return "Good Signal";
-	}
 }
+	
+	
