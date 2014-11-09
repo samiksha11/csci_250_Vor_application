3d61366ed570155009e9782ed70dbc258a09edb5
samiksha11 <samiksha.solanki11@gmail.com>
2014-11-08 19:48:10 -0800

    Added comments with RadioValues file and change with the main class as the interface class.

--- Perciatelli/src/Perciatelli/RadioValues.java
+++ Perciatelli/src/Perciatelli/RadioValues.java
@@ -1,26 +1,34 @@
+/* Team : Perciatelli
+ * IDE: NEt BEANS.
+ * Project: VOR APPLICATION.
+ * class: RADIO
+ */
 package Perciatelli;
 
 
 import java.util.Random;
 
-public class RadioValues 
+public class RadioValues      // class RadioValues
 {
+//public static void main(String[] args)
+//{
+//}
 	int test;
-	int counter = 10;
-	int spotID1 = test + 353;
-	int spotID2 = test + 242;
+	int counter = 10;               // Added the counter as 10
+	int spotID1 = test + 353;       // Final Station ID  
+	int spotID2 = test + 242;         // starting station ID
 	double startRad = 0;
 	public RadioValues(int test)
         {
 		this.test = test;
 	}
-        public double generateRadial()
+        public double generateRadial()      // Function for generating the testvalues
         {
 		double pinRadial;
 		
 		Random randomNo = new Random();
                 
-		pinRadial = randomNo.nextDouble() * 3.45;
+		pinRadial = randomNo.nextDouble() * 3.45;     // Created the random values for radial between 0-360
 		int pinMove = randomNo.nextInt(2);
 		if(pinMove > 0)
                 {
@@ -30,6 +38,41 @@ public class RadioValues
                 return 360 - pinRadial;
                 
 	}
+
+	// function for creating the Station IDs between 242 and 353 as given in hawaii chart
+	public int generateID()
+        {
+		Random rnd = new Random();
+                int a = rnd.nextInt(spotID1-spotID2) + spotID2;
+                System.out.println(a);  // print the random station Ids
+               
+		return a;
+	}
+	
+	public String generateSignalQuality() // function for creating the signal quality good or bad signal
+        {
+		if(counter <= 0)
+                {
+			return "Good signal";
+		}
+		if(this.counter < 5)
+                {
+			this.counter--;
+			System.out.println(""
+                                + "counter = " + this.counter);
+			return "bad signal";
+		}
+		
+		Random rn4 = new Random();
+		int a = rn4.nextInt(3);
+		if(a == 0)
+                {
+			this.counter--;
+			System.out.println("counter = " + this.counter);
+			return "bad signal";
+		}
+		return "Good Signal";
+	}
+        
 }
-	
-	
+
