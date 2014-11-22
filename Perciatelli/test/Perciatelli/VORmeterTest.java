e801d3c71a1c9562a27cb272e78c73c6438e3162
samiksha11 <samiksha.solanki11@gmail.com>
2014-11-21 22:16:08 -0800

    test of slop values incorrect slop found

---
+++ Perciatelli/test/Perciatelli/VORmeterTest.java
@@ -0,0 +1,129 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Perciatelli;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author samiksha solanki
+ */
+public class VORmeterTest {
+    
+    public VORmeterTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getPreferredSize method, of class VORmeter.
+     */
+    @Test
+    public void testGetPreferredSize() {
+        System.out.println("getPreferredSize");
+        VORmeter instance = new VORmeter();
+        
+        Dimension expResult = new Dimension(500,500);
+        //Dimension expResult = null;
+        Dimension result = instance.getPreferredSize();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of findPoint method, of class VORmeter.
+     */
+    @Test
+    public void testFindPoint() {
+        System.out.println("findPoint");
+        double x = 1.0;
+        double y = 1.0;
+        double distance = 10.0;
+        double slope = 1.0;
+        VORmeter instance = new VORmeter();
+        instance.findPoint(x, y, distance, slope);
+        // TODO review the generated test code and remove the default call to fail.
+       // fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of MeterCoordinate method, of class VORmeter.
+     */
+    @Test
+    public void testMeterCoordinate() {
+        System.out.println("MeterCoordinate");
+        float Meterdegree = 0.0F;
+        float r = 0.0F;
+        VORmeter instance = new VORmeter();
+        instance.MeterCoordinate(Meterdegree, r);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of MeterSlope method, of class VORmeter.
+     */
+    @Test
+    public void testMeterSlope() {
+        System.out.println("MeterSlope");
+        double x1 = 20.0;
+        double y1 = 20.0;
+        double x2 = 10.0;
+        double y2 = 20.0;
+        VORmeter instance = new VORmeter();
+        double expResult = 1.0;
+        double result = instance.MeterSlope(x1, y1, x2, y2);
+        assertEquals(expResult, result, 1.0);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+     public void testMeterSlope_1() {
+        System.out.println("MeterSlope");
+        double x1 = 30.0;
+        double y1 = 30.0;
+        double x2 = 10.0;
+        double y2 = 10.0;
+        VORmeter instance = new VORmeter();
+        double expResult = 1.0;
+        double result = instance.MeterSlope(x1, y1, x2, y2);
+        assertEquals(expResult, result, 1.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    /**
+     * Test of paintComponent method, of class VORmeter.
+     */
+    @Test
+    public void testPaintComponent() {
+        System.out.println("paintComponent");
+        Graphics g = null;
+        VORmeter instance = new VORmeter();
+        instance.paintComponent(g);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+}
